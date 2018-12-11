7831a22b91258c720ab4c90e15a8e87c95e0502e
Yevhen-H <y.hurtovyi@yahoo.com>
2018-12-11 00:04:11 -0600

    1. change methods and parameters 2. added new copy/paste transformed chunk

--- add_test_group_new.py
+++ add_test_group_new.py
@@ -14,7 +14,7 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
 
-    def login(self, wd, username="admin", password="secret"):
+    def login(self, wd, username, password):
         # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -30,30 +30,41 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username ="admin", password = "secret")
         self.open_group_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name = "Test", header = "Katalon")
         self.logout(wd)
+
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username ="admin", password = "secret")
+        self.open_group_page(wd)
+        self.create_group(wd, name = "", header = "")
+        self.logout(wd)
+
+
+    def create_group(self, wd, name, header):
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill up group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(header)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+
 
     def logout(self, wd):
         # logout
         wd.find_element_by_link_text("Logout").click()
-
-    def create_group(self, wd):
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Test")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Katalon")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Katalon 1")
-        # submit group creation
-        wd.find_element_by_name("submit").click()
 
 
     def open_group_page(self, wd):
