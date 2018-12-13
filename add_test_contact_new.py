8abd8aadb3433516398c93e01310eb5ecc104b36
Yevhen-H <y.hurtovyi@yahoo.com>
2018-12-12 21:51:03 -0600

    Created new test for Contact (refactoring to the new methods)

--- add_test_contact_new.py
+++ add_test_contact_new.py
@@ -1,51 +1,81 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from contact import Contact
+
+
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
 
-    
+
     def test_add_contact(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        self.open_home_page(wd)
+        self.login(wd, username ="admin", password ="secret")
+        self.create_contact(wd, Contact (name = "Yvhen", lastname = "Hurtovyi", company="Sidley", address="Chicago", mobile="7733311608", email="y.hurtovyi@yahoo.com"))
+        self.logout(wd)
+
+    def test_add_digits_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_contact(wd, Contact (name="1", lastname="2", company="3", address="4",
+                            mobile="5", email="6"))
+        self.logout(wd)
+
+
+
+    def open_home_page(self, wd):
+            wd.get("http://localhost/addressbook/")
+
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+
+
+    def create_contact(self, wd, contact):
+        # init contact creation
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Yvhen")
+        wd.find_element_by_name("firstname").send_keys(contact.name)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Hurtovyi")
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("Sidley")
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Chicago")
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("7733311608")
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("y.hurtovyi@yahoo.com")
+        wd.find_element_by_name("email").send_keys(contact.email)
         wd.find_element_by_name("theform").click()
+        # submit
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-    
+
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
