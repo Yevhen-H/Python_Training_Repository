0de6efcef669fe438ac8bf2d40eac3f597a4428f
Yevhen-H <y.hurtovyi@yahoo.com>
2018-12-12 21:51:21 -0600

    Created new test for Contact (refactoring to the new methods)

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+    def __init__ (self, name, lastname, company, address, mobile, email):
+        self.name = name
+        self.lastname = lastname
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.email = email
