cc05015d089a2923131b0cdddc919edb0bb1c3c6
Yevhen-H <y.hurtovyi@yahoo.com>
2018-12-16 13:12:31 -0600

    added fixture for application_contact(the last)

--- add_test_contact_new.py
+++ add_test_contact_new.py
@@ -13,7 +13,7 @@ def app (request):
 def test_add_contact(app):
     app.login(username="admin", password="secret")
     app.create_contact(
-            Contact(name="Yvhen", lastname="Hurtovyi", company="Sidley", address="Chicago", mobile="7733311608",
+            Contact(name="Yevhen", lastname="Hurtovyi", company="Sidley", address="Chicago", mobile="7733311608",
                     email="y.hurtovyi@yahoo.com"))
     app.logout()
 
@@ -23,7 +23,3 @@ def test_add_digits_contact(app):
     app.create_contact(Contact(name="1", lastname="2", company="3", address="4",
                                     mobile="5", email="6"))
     app.logout()
-
-
-if __name__ == "__main__":
-    unittest.main()
