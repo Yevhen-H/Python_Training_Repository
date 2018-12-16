c69f0842e7c7fc3321343d8f6255a59587c2b3f4
Yevhen <y.hurtovyi@yahoo.com>
2018-12-16 11:29:05 -0600

    39 line was changed for more short one

--- add_test_contact_new.py
+++ add_test_contact_new.py
@@ -4,30 +4,27 @@ import unittest
 from contact import Contact
 
 
-
 class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
 
-
     def test_add_contact(self):
-        self.login(username ="admin", password ="secret")
-        self.create_contact(Contact (name = "Yvhen", lastname = "Hurtovyi", company="Sidley", address="Chicago", mobile="7733311608", email="y.hurtovyi@yahoo.com"))
+        self.login(username="admin", password="secret")
+        self.create_contact(
+            Contact(name="Yvhen", lastname="Hurtovyi", company="Sidley", address="Chicago", mobile="7733311608",
+                    email="y.hurtovyi@yahoo.com"))
         self.logout()
 
     def test_add_digits_contact(self):
         self.login(username="admin", password="secret")
-        self.create_contact(Contact (name="1", lastname="2", company="3", address="4",
-                            mobile="5", email="6"))
+        self.create_contact(Contact(name="1", lastname="2", company="3", address="4",
+                                    mobile="5", email="6"))
         self.logout()
-
-
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
 
     def login(self, username, password):
         wd = self.wd
@@ -39,9 +36,7 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
-
+        wd.find_element_by_xpath("//input[@value='Login']").click() 
 
     def create_contact(self, contact):
         wd = self.wd
@@ -71,25 +66,27 @@ class TestAddContact(unittest.TestCase):
         # submit
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-
     def logout(self):
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
-    
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
