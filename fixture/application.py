4de79056f6bdd1cd63aff5c61fb1d614ccebaacd
Yevhen-H <y.hurtovyi@yahoo.com>
2018-12-17 12:40:50 -0600

    Was added a new file session.py for "fixtuer's" package

--- fixture/application.py
+++ fixture/application.py
@@ -1,30 +1,19 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-
+from fixture.session import SessionHelper
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
 
     def open_group_page(self):
@@ -50,9 +39,6 @@ class Application:
         # submit group creation
         wd.find_element_by_name("submit").click()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
 
     def destroy(self):
