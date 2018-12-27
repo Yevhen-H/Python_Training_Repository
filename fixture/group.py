2d5727bdf1c46ebc5500995092c9b2bf0f75f266
Yevhen-H <y.hurtovyi@yahoo.com>
2018-12-26 22:45:38 -0600

    Were replaced Login and Logout to fixture(deleted from all tests)

--- fixture/group.py
+++ fixture/group.py
@@ -14,7 +14,6 @@ class GroupHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
-
     def fill_group_form(self, group):
         wd = self.app.wd
         self.change_field_value("group_name", group.name)
@@ -44,7 +43,6 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
-
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_group_page()
@@ -56,7 +54,6 @@ class GroupHelper:
         #submit modification
         wd.find_element_by_name("update").click()
         self.return_to_the_group_page()
-
 
     def return_to_the_group_page(self):
         wd = self.app.wd
