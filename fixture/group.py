da64bf5b60661fc1506813c190b4f6475004baf3
Yevhen-H <y.hurtovyi@yahoo.com>
2019-01-22 20:31:40 -0600

    Added randomness in "group" for (Ex 13)

--- fixture/group.py
+++ fixture/group.py
@@ -32,10 +32,18 @@ class GroupHelper:
         self.return_to_the_group_page()
         self.group_cache = None
 
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
+
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_group_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_the_group_page()
@@ -45,11 +53,13 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
-    def modify_first_group(self, new_group_data):
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_group_page()
-        self.select_first_group()
-        #open modification form
+        self.select_group_by_index(index)
         wd.find_element_by_name("edit").click()
         # fill group page
         self.fill_group_form(new_group_data)
